3e678b0f6444240645de6e2563f0fbfd2c9d8fe1
TaliaFXY0828 <cindyfung0828@gmail.com>
2026-09-24 08:18:58 +0000

    [skip actions] lab/lab04/ch04_t14_elif.py

--- lab/lab04/ch04_t12_if_having.py
+++ lab/lab04/ch04_t12_if_having.py
@@ -1,10 +1,10 @@
 def using_control_once() -> str:
-    if 2 > 1:
+    if 8 > 3:
         return "Success #1"
 
 
 def using_control_again() -> str:
-    if 7 == 2:
+    if 7 == 7:
         return "Success #2"
 
 
